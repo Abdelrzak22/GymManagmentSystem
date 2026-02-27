fd463edb78f5463d14ad7e0cf0caedf1c6a14cd8
Abdelrazzaq <abdotaha12345m@gmail.com>
2026-02-27 18:51:50 +0200

    Handle  model creating

---
+++ GymManagmentDAL/Entities/GymUser.cs
@@ -0,0 +1,31 @@
+﻿using GymManagmentDAL.Entities.Enum;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagmentDAL.Entities
+{
+    internal abstract class GymUser:BaseEntity
+
+    {
+
+        public string Name { get; set; } = null!;
+        public string Email { get; set; }=null!;
+        public string Phone { get; set; } = null!;
+        public DateOnly Dateofbirth { get; set; }
+        public Gender Gender { get; set; }
+        public Address Address { get; set; }= null!;
+    }
+
+    [Owned]
+    public class Address
+    {
+        public string Street { get; set; } = null!;
+        public int BuldingNumber { get; set; }
+        public string City { get; set; } = null!;
+    }
+}
---
+++ GymManagmentDAL/Entities/Trainer.cs
@@ -0,0 +1,16 @@
+﻿using GymManagmentDAL.Entities.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagmentDAL.Entities
+{
+    internal class Trainer:GymUser
+    {
+        //Hire Date==created at
+        public Specialties specialties {  get; set; }
+
+    }
+}
